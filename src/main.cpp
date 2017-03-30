6ae7b5529b059deff4899c09e3d7bc77eda1cc63
Your Name <you@example.com>
2017-03-30 18:02:02 +0200

    chaud patate part 2

--- src/main.cpp
+++ src/main.cpp
@@ -2,44 +2,47 @@
 #include <stdio.h>
 #include "jpg.h"
 #include "mnist.h"
-
+#include "limits.h"
 
 
 float dist(float* v1,float* v2) {
       float d=0;
       for(int i=0;i<784;i++){
-      d += (v1[i]-v2[i]* v1[i]-v2[i]);
+      	d += (v1[i]-v2[i])*(v1[i]-v2[i]);
+	}
+		return d;
 
 }
-return 0;
+
+int lin_class(float* w,float* x) {
+    float d=0;
+    for(i=0;i<784;i++);
+    d +=w[i]*x[i];
+    }
+     if(d>0) return 1;
+     else return 0;
 
 }
+
 
 int main(int argc, char** argv){
     
-
     float** images = read_mnist("train-images.idx3-ubyte");
     float* labels = read_labels("train-labels.idx1-ubyte");
-    float** test_images = read_mnist("10k-images.idx3-ubyte");
-
-
-    for(int i=0; i<60000; i++) {
-    printf("%u/n",i);  
-    float mind=1;
-    int MN;
-    for(int j=0; j<60000; j++) {
-        float d=dist(test_images[i],images[j]);
-        if(d<=mind  || mind==-1) {
-         mind=d;
-         MN=j;
-      }
-
-}
-
-    int inf = labels[MN];
-    save_jpg(test_images[i],28,28,"%u/%u.jpg",inf,i);
-             
-}
+    float** test_images = read_mnist("t10k-images.idx3-ubyte");
+    float* test_labels = read_labels("t10k-labels.idx1-ubyte");
+    
+    float* w=new float[784]
+            for(i=0;i<784;i++) w[i]=(float)rand()*2/INT_MAX;
+            float E=0;
+	        for(int i=0; i<10000; i++) {
+	        printf("u%\n",i);
+     int inf =lin_class(w,test_image[i]); 	  
+//    	save_jpg(test_images[i],28,28,"%u/%u.jpg",inf,i);
+// if(inf!=test_labels[i]) E++;      
+	    printf("Erreur =%0.2f%%\n",(E*100)/i);  
+		
+ }  
 
     return 0;
 }
