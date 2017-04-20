d17152f23dd0ce0d03db5b2571e1eb59a3227ac9
Your Name <you@example.com>
2017-04-20 17:30:17 +0200

    TD4

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,68 @@
-#include "math.h"
-#include <stdio.h>
 #include "jpg.h"
 #include "mnist.h"
-#include "limits.h"
+#include<limits.h>
 
-
-float dist(float* v1,float* v2) {
-      float d=0;
-      for(int i=0;i<784;i++){
-      	d += (v1[i]-v2[i])*(v1[i]-v2[i]);
+float dist(float* v1, float* v2){
+	float d=0;
+	for(int i=0; i<784; i++){
+		d+=(v1[i]-v2[i])*(v1[i]-v2[i]);
 	}
-		return d;
-
+	return d;
 }
 
-int lin_class(float* w,float* x) {
-    float d=0;
-    for(i=0;i<784;i++);
-    d +=w[i]*x[i];
-    }
-     if(d>0) return 1;
-     else return 0;
-
+int lin_class(float *w, float *x){
+	float d = 0;
+	for(int  i=0; i<784; i++){
+			d+= w[i] *x[i];
+	}
+	if(d>=0){
+		return 1;
+	}else{
+		return -1;
+	}
 }
 
+int main()
+{
 
-int main(int argc, char** argv){
-    
+	
     float** images = read_mnist("train-images.idx3-ubyte");
-    float* labels = read_labels("train-labels.idx1-ubyte");
-    float** test_images = read_mnist("t10k-images.idx3-ubyte");
-    float* test_labels = read_labels("t10k-labels.idx1-ubyte");
-    
-    float* w=new float[784]
-            for(i=0;i<784;i++) w[i]=(float)rand()*2/INT_MAX;
-            float E=0;
-	        for(int i=0; i<10000; i++) {
-	        printf("u%\n",i);
-     int inf =lin_class(w,test_image[i]); 	  
-//    	save_jpg(test_images[i],28,28,"%u/%u.jpg",inf,i);
-// if(inf!=test_labels[i]) E++;      
-	    printf("Erreur =%0.2f%%\n",(E*100)/i);  
-		
- }  
+	float* labels = read_labels("train-labels.idx1-ubyte");
+	float** test_images = read_mnist("t10k-images.idx3-ubyte");
+	float* test_labels = read_labels("t10k-labels.idx1-ubyte");
+	float err=0;
+	float *w = new float[784];
 
+    //STEP 1 : INITIALISATION
+	for(int i =0; i<784;i++){
+		w[i]=(float)rand()*2/INT_MAX-1;
+	}
+        
+    float gamma=0.01; 
+
+     //STEP 2: LEARNING
+    for(int i=0;i<60000;i++){
+        int prediction=lin_class(w,images[i]);
+		int verite=(labels[i]==1)?1:-1;
+         if(verite != prediction) {
+            printf("ERREUR\n");
+ 	        for(int j=0;j<784;j++){
+    		     w[j]=w[j] +gamma*verite*images[i][j];
+	        }
+		  }
+     }
+
+
+    for(int i=0; i<10000; i++) {
+		int inf = lin_class(w, test_images[i]); ;
+		printf("%u\n", i);
+        //save_jpg(test_images[i], 28, 28, "%u/%04u.jpg", inf, i);
+		if((inf ==1 && test_labels[i]!= 1)||( inf==-1 && test_labels[i]== 1)){
+			err++;
+		}
+	printf("Il y a %0.2f%% d'erreur. \n", (err*100)/i);
+    }
+	printf("Il y a %d merreur. \n", (int)err);
     return 0;
 }
+
